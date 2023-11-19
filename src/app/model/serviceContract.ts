0516fc961f07da8615908a4acc97b82ca415ce26
luis_ <luis.bendez342@gmail.com>
2023-11-19 18:59:44 -0500

    algunos cambios de nombre de columnas y botones

--- src/app/model/serviceContract.ts
+++ src/app/model/serviceContract.ts
@@ -2,6 +2,6 @@ import { Timestamp, timestamp } from "rxjs";
 
 export class serviceContractModel {
     employer_id: number = 0;
-    service_date: string = '';
+    service_date:Date = new Date(Date.now())
     desc_service: string = '';
 }
